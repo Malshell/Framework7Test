6b7b65130e25c7f01c49cbfda66afb9cb8f9f081
moyong <965868297@qq.com>
2017-06-05 11:19:06 +0800

    first commit

---
+++ WebContent/dist/js/my-app.js
@@ -0,0 +1,74 @@
+// Initialize your app
+var myApp = new Framework7({
+	modalTitle:"testApp",
+	  preprocess: function (content, url, next) {
+          if (url === 'people.html') {
+              var template = Template7.compile(content);
+              var resultContent = template({
+                  title: 'People',
+                  people: ['John', 'Ivan', 'Mary']
+              })
+              return resultContent;
+          }
+      }
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+$$('.alert-text').on('click', function () {
+    myApp.alert('Here goes alert text');
+});
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+    
+    if (page.name === 'about') {
+      // Following code will be executed for page with data-page attribute equal to "about"
+      myApp.alert('Here comes About page');
+    }
+  })
+
+// Callbacks to run specific code for specific pages, for example for About page:
+myApp.onPageInit('about', function (page) {
+    // run createContentPage func after link was clicked
+    $$('.create-page').on('click', function () {
+        createContentPage();
+    });
+});
+
+
+// Generate dynamic page
+var dynamicPageIndex = 0;
+function createContentPage() {
+	mainView.router.loadContent(
+        '<!-- Top Navbar-->' +
+        '<div class="navbar">' +
+        '  <div class="navbar-inner">' +
+        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="pages">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div data-page="dynamic-pages" class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '      <div class="content-block">' +
+        '        <div class="content-block-inner">' +
+        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+        '        </div>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>'
+    );
+	return;
+}
